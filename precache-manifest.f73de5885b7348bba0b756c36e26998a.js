1fc651763b5e52f0c1a87552161c3c062ede25a1
Shivam Agrawal <shivam301296@gmail.com>
2020-02-10 19:47:52 +0530

    Updates

---
+++ precache-manifest.f73de5885b7348bba0b756c36e26998a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a5bc963d4a2342343787a2f71bce50",
+    "url": "/postmanweb/index.html"
+  },
+  {
+    "revision": "2312723216c60875a9ce",
+    "url": "/postmanweb/static/css/main.d2d74fe7.chunk.css"
+  },
+  {
+    "revision": "6f322dff2201dee4bb68",
+    "url": "/postmanweb/static/js/2.66f0794a.chunk.js"
+  },
+  {
+    "revision": "395404804f0eba41b11435acf8e831a6",
+    "url": "/postmanweb/static/js/2.66f0794a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2312723216c60875a9ce",
+    "url": "/postmanweb/static/js/main.008dcbca.chunk.js"
+  },
+  {
+    "revision": "fd96ed87b85544e81dc3",
+    "url": "/postmanweb/static/js/runtime-main.e74ba4da.js"
+  }
+]);
